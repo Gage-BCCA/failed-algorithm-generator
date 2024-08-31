0bfb485701fe8cd349e8d009bede79308e7c8ee9
Gage <gschaffer@basecampcodeacademy.org>
2024-08-31 11:33:59 -0500

    Added portfolio page with formatting. Deleted Robots section. Added minor Javascript

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("bundle.css");
+    eleventyConfig.addPassthroughCopy("js/test.js");
+    eleventyConfig.addPassthroughCopy("img/");
     eleventyConfig.addPlugin(syntaxHighlight);
   };
---
+++ js/test.js
@@ -0,0 +1,40 @@
+// Add event listeners to the skill cards
+const skillCards = document.getElementsByClassName("skill-card");
+for (let card of skillCards) {
+  console.log(card);
+  card.addEventListener("mouseover", function focusElement() {
+    let cardHeader = this.querySelector("h2");
+
+    this.style.transitionDuration = "200ms";
+    this.style.backgroundColor = "#e74c3c";
+    this.style.padding = "20px";
+
+    cardHeader.style.transitionDuration = "400ms";
+    cardHeader.style.fontSize = "2rem";
+
+
+  });
+
+  card.addEventListener("mouseout", function unfocusElement(e) {
+    this.style.backgroundColor = "#2c3e50";
+    this.style.padding = "10px";
+
+    let cardHeader = this.querySelector("h2");
+    cardHeader.style.transitionDuration = "200ms";
+    cardHeader.style.fontSize = "1.5rem";
+  });
+
+}
+
+function showDetails(elem) {
+  let projectDetailsId = "project-details-" + elem.id;
+  let details = document.getElementById(projectDetailsId);
+  if (details.style.display === "none") {
+    console.log("DISPLAYING");
+    details.style.display = "block";
+    elem.textContent = "Show Less";
+  } else {
+    details.style.display = "none";
+    elem.textContent = "Show More";
+  }
+}
