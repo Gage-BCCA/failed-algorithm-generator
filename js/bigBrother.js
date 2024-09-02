d84e567a4e2f764c894cdc94d676b7452d784e70
Gage <gschaffer@basecampcodeacademy.org>
2024-09-01 21:26:32 -0500

    Started class for a website view

--- js/bigBrother.js
+++ js/bigBrother.js
@@ -1,7 +1,18 @@
+class View {
+  constructor(navigatorUserAgentDataObject){
+    this.timestamp = new Date();
+    this.platform = navigatorUserAgentDataObject.platform;
+    this.mobile = navigatorUserAgentDataObject.mobile; 
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(recordView, 2000);
   });
 
 function recordView() {
-    console.log("View recorded.")
+    console.log("View recorded.");
+    console.log(navigator.userAgentData.platform);
+    let date = new Date();
+    console.log(date);
 }
