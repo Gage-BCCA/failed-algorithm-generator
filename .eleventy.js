ad39133048d38b9cfd34e8d2e03f53c4d0ba301c
Gage <gschaffer@basecampcodeacademy.org>
2024-09-01 21:14:56 -0500

    Added some additional Javascript for anonymous analytics

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,8 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("bundle.css");
-    eleventyConfig.addPassthroughCopy("js/test.js");
+    eleventyConfig.addPassthroughCopy("js/portfolio.js");
+    eleventyConfig.addPassthroughCopy("js/bigBrother.js");
     eleventyConfig.addPassthroughCopy("img/");
     eleventyConfig.addPlugin(syntaxHighlight);
   };
---
+++ js/bigBrother.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(recordView, 2000);
+  });
+
+function recordView() {
+    console.log("View recorded.")
+}
